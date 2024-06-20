d41c9b6b1040b7d8b6608062deb99e8161630e6a
Titamik <titamik777@yandex.ru>
2024-06-20 23:28:38 +0300

    Project, Team, User, Task, Comment Factories

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\TaskStatus;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::query()->inRandomOrder()->first()->id,
+            'task_status_id' => TaskStatus::query()->inRandomOrder()->first()->id,
+            'title' => fake()->sentence,
+            'description' => fake()->text,
+            'order' => 500
+        ];
+    }
+}
